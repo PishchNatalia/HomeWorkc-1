583e8544aea9dc35b32084cddaaa709198d71df2
PishchNatalia <n.pisch@mail.ru>
2022-12-02 12:38:39 +0300

    Задача1

---
+++ Задача2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Домашняя работа
+//Задача 2
+
+Console.Write("Imput a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Imput a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Imput a third number: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+    if (num1>num2 & num1>num3)
+{
+    Console.WriteLine("Максимальное число num1");
+}
+    if (num2>num1 & num2>num3)
+{
+    Console.WriteLine("Максимальное число num2");
+}
+    else
+    
+   Console.WriteLine("Максимальное число num3");
+
+    
